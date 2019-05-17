773290cbbdcd102eb7ce522afa4ad7e931b27732
Pascal Bothner <pascal.bothner@outlook.de>
2019-05-17 17:52:52 +0200

    added shared module and splittet navbar into own component

--- admin-app/admin-app-fe/src/app/home/home.module.ts
+++ admin-app/admin-app-fe/src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
 import { homeRoutes } from './home.routes';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(homeRoutes)
   ],
   declarations: [HomeComponent]
