66b52f499b1e4c3e3fdff9f28957e9780835967a
Pascal Bothner <pascal.bothner@outlook.de>
2019-06-02 16:38:03 +0200

    added filter ability

--- admin-app/admin-app-fe/src/app/ressources/components/search/search.component.ts
+++ admin-app/admin-app-fe/src/app/ressources/components/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Component, Output, EventEmitter, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Tag, FilterTag } from 'src/app/shared/models/tag.model';
 
 @Component({
   selector: 'app-search',
@@ -7,17 +8,25 @@ import { Validators, FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   @Output()
   public submitSearchValue: EventEmitter<string> = new EventEmitter<string>();
+
+  @Output()
+  public selectedFilterValue: EventEmitter<FilterTag> = new EventEmitter<FilterTag>();
+
+  @Input()
+  public filters: FilterTag[];
 
   public searchForm: FormGroup = this.fb.group({ search: [''] });
 
   constructor(private readonly fb: FormBuilder) {}
 
-  ngOnInit() {}
-
   public onSearchKeyup(): void {
     this.submitSearchValue.emit(this.searchForm.value);
+  }
+
+  public selectFilter(filter: FilterTag): void {
+    this.selectedFilterValue.emit({ ...filter, isSelected: true });
   }
 }
--- admin-app/admin-app-fe/src/app/shared/models/tag.model.ts
+++ admin-app/admin-app-fe/src/app/shared/models/tag.model.ts
@@ -3,4 +3,8 @@ export interface Tag {
   type: TagType;
 }
 
+export interface FilterTag extends Tag {
+  isSelected?: boolean;
+}
+
 export type TagType = 'FRONTEND' | 'BACKEND' | 'INFRASTRUCTURE' | 'DESIGN' | 'ARCHITECTURE';
