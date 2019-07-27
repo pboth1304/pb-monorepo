cc06c239ffe59c8fbbafc1693f6e1f4787afb41c
pascalbothner <pascal.bothner@outlook.de>
2019-07-27 12:45:30 +0200

    feat(admin-app): changed globalPrefix url of api

--- apps/admin-app-api/src/main.ts
+++ apps/admin-app-api/src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1/admin';
   app.setGlobalPrefix(globalPrefix);
 
   // TODO remove / refactor
