360cd66bdc74ad1f4cc06e997a88737588e93807
Pascal Bothner <pascal.bothner@outlook.de>
2019-09-14 15:10:22 +0200

    feat(travelency): fixed tests

--- apps/travelency-api/src/app/hotels/services/hotels.service.spec.ts
+++ apps/travelency-api/src/app/hotels/services/hotels.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HotelsService } from './hotels.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('HotelsService', () => {
   let service: HotelsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HotelsService],
+      providers: [
+        HotelsService,
+        {
+          provide: getModelToken('Hotel'),
+          useValue: {}
+        }
+      ]
     }).compile();
 
     service = module.get<HotelsService>(HotelsService);
